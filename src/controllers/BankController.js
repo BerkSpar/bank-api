2352c1759de925cf582ebc13b9c70a427da53263
Felipe Passos <berkspar@gmail.com>
2021-01-08 14:58:45 -0300

    Update BankController.js

--- src/controllers/BankController.js
+++ src/controllers/BankController.js
@@ -1,5 +1,43 @@
 const database = require('../database/connection')
 
+async function getUsuario(usuario_id) {
+    const sql = `SELECT * FROM usuario WHERE id = ${usuario_id}`;
+
+    try {
+        const rows = await database.query(sql)
+
+        return rows[0][0]
+    } catch (error) {
+        console.log(error.sqlMessage)
+        throw error
+    }
+}
+
+async function geraMovimentacao(usuario_id, valor, tipo_movimento, codigo = null) {
+    const sql =
+        `INSERT INTO movimentacao (
+            valor,
+            tipo_movimento,
+            usuario_id,
+            data,
+            observacao
+        ) VALUES (
+            ${valor},
+            '${tipo_movimento}',
+            ${usuario_id},
+            now(),
+            ${codigo == null ? null : `"${codigo}"`}
+        );`
+
+
+    try {
+        const rows = await database.query(sql)
+    } catch (error) {
+        console.log(error.sqlMessage)
+        throw e
+    }
+}
+
 class BankController {
     async deposita(request, response) {
         const { usuario_id, valor } = request.body
@@ -9,22 +47,11 @@ class BankController {
             return
         }
 
-        const sql =
-            `INSERT INTO movimentacao (
-                valor,
-                tipo_movimento,
-                usuario_id,
-                data
-            ) VALUES (
-                ${valor},
-                'DEPÓSITO',
-                ${usuario_id},
-                now()
-            );`
-
+        const sql = `UPDATE usuario SET saldo = (saldo + ${valor}) WHERE id = ${usuario_id}`
 
         try {
-            const rows = await database.query(sql)
+            await database.execute(sql)
+            await geraMovimentacao(usuario_id, valor, 'DEPOSITO')
 
             response.json({ message: 'Depositado com sucesso!' })
         } catch (error) {
@@ -33,12 +60,68 @@ class BankController {
 
     }
 
-    saque(request, response) {
-        response.send('Em implementação...')
+    async saque(request, response) {
+        const { usuario_id, valor } = request.body
+
+        if (usuario_id == undefined || valor == undefined) {
+            response.json({ message: "Houve um erro ao receber os valores" })
+            return
+        }
+
+        var usuario = {}
+        try {
+            usuario = await getUsuario(usuario_id)
+        } catch (error) {
+            response.json({ message: error.sqlMessage })
+            return
+        }
+
+        if (usuario.saldo - valor <= 0) {
+            response.json({ message: 'Saldo inválido' })
+            return
+        }
+
+        const sql = `UPDATE usuario SET saldo = (saldo - ${valor}) WHERE id = ${usuario_id}`
+        try {
+            await database.execute(sql)
+            await geraMovimentacao(usuario_id, valor, 'SAQUE')
+
+            response.json({ message: 'Saque realizado com sucesso' })
+        } catch (error) {
+            response.json({ message: error.sqlMessage })
+        }
     }
 
-    pagamento(request, response) {
-        response.send('Em implementação...')
+    async pagamento(request, response) {
+        const { usuario_id, valor, codigo } = request.body
+
+        if (usuario_id == undefined || valor == undefined || codigo == undefined) {
+            response.json({ message: "Houve um erro ao receber os valores" })
+            return
+        }
+
+        var usuario = {}
+        try {
+            usuario = await getUsuario(usuario_id)
+        } catch (error) {
+            response.json({ message: error.sqlMessage })
+            return
+        }
+
+        if (usuario.saldo - valor <= 0) {
+            response.json({ message: 'Saldo inválido' })
+            return
+        }
+
+        const sql = `UPDATE usuario SET saldo = (saldo - ${valor}) WHERE id = ${usuario_id}`
+        try {
+            await database.execute(sql)
+            await geraMovimentacao(usuario_id, valor, 'PAGAMENTO', `Código do boleto: ${codigo}`)
+
+            response.json({ message: 'Pagamento realizado com sucesso' })
+        } catch (error) {
+            response.json({ message: error.sqlMessage })
+        }
     }
 
     transferencia(request, response) {
@@ -56,7 +139,7 @@ class BankController {
 
             const rows = await database.query(sql)
 
-            response.json(rows[0])
+            response.json(rows[0][0])
         } catch (error) {
             response.json({ message: error.sqlMessage })
         }
@@ -69,7 +152,7 @@ class BankController {
         }
 
         try {
-            const sql = `SELECT * FROM movimentacaso WHERE usuario_id = ${request.query.usuario_id}`
+            const sql = `SELECT * FROM movimentacao WHERE usuario_id = ${request.query.usuario_id}`
 
             const rows = await database.query(sql)
 
